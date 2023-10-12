01914384975fea532361bbe2d5a2961d2d144393
Fenish <sohretalhadev@outlook.com>
2023-10-13 02:10:27 +0300

    Add registration

--- SessionAuthentication/Controllers/Authentication.cs
+++ SessionAuthentication/Controllers/Authentication.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using SessionAuthentication.Database;
+using SessionAuthentication.Database.Datasets;
+using SessionAuthentication.Utils;
 
 namespace SessionAuthentication.Controllers;
 
+
+[ApiController]
 [Route("[controller]")]
-public class Authentication: ControllerBase
+public class Authentication(DataContext context) : ControllerBase
 {
+    public class RegistrationRequest
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+    }
+    
     [HttpPost("/login")]
     public ActionResult Login()
     {
@@ -12,8 +24,35 @@ public class Authentication: ControllerBase
     }  
     
     [HttpPost("/register")]
-    public ActionResult Register()
+    public ActionResult Register([FromBody] RegistrationRequest request)
     {
-        return Ok("Registered");
+        // Check if user already exists
+        // If user exists, return 400 Bad Request
+        var isUserExists = context.Users.Any(user => user.Username == request.Username || user.Email == request.Email);
+        if (isUserExists)
+        {
+            return BadRequest(new { message = "User already exists" });
+        }
+        
+        // Hash password for security
+        // Im using SHA256 here, but you can use any other hashing algorithm
+        var hashedPassword = StringUtils.Sha256Encrypt(request.Password);
+        
+        // Create new user object
+        var user = new User
+        {
+            Username = request.Username,
+            Password = hashedPassword,
+            Email = request.Email
+        };
+        
+        // Save user to database
+        context.Users.Add(user);
+        
+        // Commit changes
+        context.SaveChanges();
+        
+        // Return success message
+        return Ok(new {message = "User created successfully" });
     }  
 }
