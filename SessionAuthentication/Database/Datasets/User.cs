5ad15c2a054e411a88b57606a7692e1c94ad3557
Fenish <sohretalhadev@outlook.com>
2023-10-13 01:09:07 +0300

    Add User Dataset

---
+++ SessionAuthentication/Database/Datasets/User.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SessionAuthentication.Database.Datasets;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+}
