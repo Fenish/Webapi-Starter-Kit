3c74422a29eced0df5667faf09d2f00c6f3599d3
Fenish <sohretalhadev@outlook.com>
2023-10-12 21:38:43 +0300

    Remove unnecessary code

--- SessionAuthentication/Program.cs
+++ SessionAuthentication/Program.cs
@@ -2,9 +2,8 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Session Authentication Api", Version = "v1", Description = "A simple example of ASP.NET Core Web API"});
@@ -17,7 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllers();
+app.UseHttpsRedirection();
 app.Run();
